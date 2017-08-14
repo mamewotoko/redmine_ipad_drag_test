7d649937ce3950784bf3ab40ee68cb13a3760832
Takashi Masuyama <mamewotoko@gmail.com>
2017-08-14 19:15:22 +0900

    remove unused import

--- libs/ParametrizedTestCase/ParametrizedTestCase.py
+++ libs/ParametrizedTestCase/ParametrizedTestCase.py
@@ -1,12 +1,16 @@
+
+# -*- coding: utf-8 -*-
+
 import unittest
-from unittest import TestLoader, TestSuite, TestCase
 
 # http://eli.thegreenplace.net/2011/08/02/python-unit-testing-parametrized-test-cases
+
 
 class ParametrizedTestCase(unittest.TestCase):
     """ TestCase classes that want to be parametrized should
         inherit from this class.
     """
+
     def __init__(self, methodName='runTest', param=None):
         super(ParametrizedTestCase, self).__init__(methodName)
         self.param = param
