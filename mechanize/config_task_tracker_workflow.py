500676ab3245de73fc129c7af4a57a74066b299b
Takashi Masuyama <mamewotoko@gmail.com>
2018-06-08 08:16:01 +0900

    add script to load default language setting

--- mechanize/config_task_tracker_workflow.py
+++ mechanize/config_task_tracker_workflow.py
@@ -1,52 +1,42 @@
-
+#! /usr/bin/env python
 # -*- coding: utf-8 -*-
 import mechanize
 import sys
 import config
 
+
 def config_task_tracker_workflow():
     br = mechanize.Browser()
-    br.open(config.REDMINE_URL)
+    try:
+        br.open(config.REDMINE_URL)
+        #print(cont.read())
+        #print("----------")
+        br.follow_link(text="Sign in")
+        #print(cont.read())
+        br.select_form(nr=2)
+        br['username'] = 'admin'
+        br['password'] = 'admin'
+        br.submit()
 
-    br.follow_link(text="Sign in")
-    br.select_form(nr=1)
-    br['username'] = 'admin'
-    br['password'] = 'admin'
-    br.submit()
+        # change password
+        br.select_form(nr=2)
+        br['password'] = 'admin'
+        br['new_password'] = 'adminadmin'
+        br['new_password_confirmation'] = 'adminadmin'
+        br.submit()
+        
+        # load english default settings        
+        cont = br.follow_link(text="Administration")
+        br.select_form(nr=2)
+        # default is english
+        br.submit()
 
-    br.follow_link(text="Administration")
-
-    # br.select_form(nr=1)
-    # # select language
-    # br.submit()
-    # return True
-    br.follow_link(text="Trackers")
-
-    br.follow_link(text="Edit")
-
-    br.select_form(nr=1)
-    br.submit()
-
-    br.select_form(nr=2)
-
-    br.form.find_control(type="checkbox", name="transitions[1][1][always]").items[0].selected = True
-    br.form.find_control(type="checkbox", name="transitions[1][2][always]").items[0].selected = True
-    br.form.find_control(type="checkbox", name="transitions[1][5][always]").items[0].selected = True
-
-    br.form.find_control(type="checkbox", name="transitions[2][1][always]").items[0].selected = True
-    br.form.find_control(type="checkbox", name="transitions[2][2][always]").items[0].selected = True
-    br.form.find_control(type="checkbox", name="transitions[2][5][always]").items[0].selected = True
-
-    br.form.find_control(type="checkbox", name="transitions[5][1][always]").items[0].selected = True
-    br.form.find_control(type="checkbox", name="transitions[5][2][always]").items[0].selected = True
-    br.form.find_control(type="checkbox", name="transitions[5][5][always]").items[0].selected = True
-    
-    br.submit()
-    
-#    print(br.response().read())    
+    except:
+        print(br.response().read())
+        
 
 if __name__ == '__main__':
     if len(sys.argv) == 2:
         config.REDMINE_URL = sys.argv[1]
-    
+
     config_task_tracker_workflow()
